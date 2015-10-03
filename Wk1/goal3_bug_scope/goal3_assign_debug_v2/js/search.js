6f102e1158db3ef851da7abde798af4820f98775
Fred-Curtis <fredlewishg@gmail.com>
2015-10-03 12:59:22 -1000

    two thirds through assignment

--- Wk1/goal3_bug_scope/goal3_assign_debug_v2/js/search.js
+++ Wk1/goal3_bug_scope/goal3_assign_debug_v2/js/search.js
@@ -5,36 +5,36 @@
 (function(){
 	
 	// Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
-	var resultsDIV = document.getElementById("results"),
-		searchInput = document.forms[0].search,
+	var resultsDIV = document.getElementById("results"),           // establishing a variable of resultsDIV given the value of document.getElementById which means it is finding an element by the element id of "results" on the web page
+		searchInput = document.forms[0].search,// establishing a variable of searchInput given a value of document.forms[0].search which means it returns all <form> elements, then the .search method searches for the specified value, and returns the position of the match
 		currentSearch = ''
-	;
+	;    // a variable of currentSearch with no value
 	
 	// Validates search query
-	var validqte == function(query){
+	var validqte == function(query){        // this line sets the variable validate )spellt incorrectly I assume) with == (equalling) a function with a parameter of query
 		
 		// Trim whitespace from start and end of search query
-		while(query.charAt(0) = " "){
-			query = query.substring(1, query.length);
+		while(query.charAt(0) = " "){    // this is saying that the first space is blank, moving the cursor and first thing typed over, creating whitespace........charAt returns the first character of a string
+			query = query.substring(1, query.length);  //...query will equal the first character of the string and the number of characters in the string
 		};
 		while(query.charAt(query.length-1) === ""){
 			query = query.substring(0, query.length-1);
-		;
+		;  // this states that there that there will be whitespace at the end of the box as well
 		
 		// Check search length, must have 3 characters
 		if(query.length < 3){
 			alert("Your search query is too small, try again.);
-			
+			// This says if the length of the value input is less than three, show the alert "Your search query is too small, try again.
 			// (DO NOT FIX THE LINE DIRECTLY BELOW)
-			searchInput.focus();
-			return;
+			searchInput.focus();   // this gives focus to the variable
+			return;                // this return statement should be returning something, but its alone?
 		};
 		
 		search(query);
 	};
 	
 	// Finds search matches
-	var search = function(query)
+	var search = function(query)     // establishes that var search is the query function
 		
 		// split the user's search query string into an array
 		var queryArray = query.join(" ");
@@ -43,44 +43,45 @@
 		var results = [];
 
 		// loop through each index of db array
-		for(var i=0, j=db.length; i<j; i++){
+		for(var i=0, j=db.length; i<j; i++){   // this says the value and length and to count up by one each time
 		
 			// each db[i] is a single video item, each title ends with a pipe "|"
 			// save a lowercase variable of the video title
-			var dbTitleEnd = db[i].indexOf('|');
+			var dbTitleEnd = db[i].indexOf('|'); // it will search the string for '|'
 			var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
-			
+			// this says to convert the string to lowercase and extract both the 0 position and variable dbTitleEnd's value
 			// loop through the user's search query words
 			// save a lowercase variable of the search keyword
 			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
-				var qitem = queryArray[ii].tolowercase();
+				var qitem = queryArray[ii].tolowercase();  // this says to send the query lowercased and to count up one each time
 				
 				// is the keyword anywhere in the video title?
 				// If a match is found, push full db[i] into results array
-				var compare = dbitem.indexOf(qitem);
-				if(compare !== -1){
+				var compare = dbitem.indexOf(qitem); // this establishes that var compare is equal to dbitem (searching for an index of qitem)
+				if(compare !== -1){   // if compare is not equal to in number or type -1
 					results.push(db[i]);
-				};
+				};                   // push results to the db info area
 			;
 		;
 		
-		results.sort();
+		results.sort();  // sort the results of var results
 		
 		// Check that matches were found, and run output functions
 		if(results.length = 0){
-			noMatch();
+			noMatch(); // if the length is not long enough noMatch..
 		}else{
-			showMatches(results);
+			showMatches(results);  // if its long enough showMatches
 		};
 	};
 	
 	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
-	var noMatch = function(){
-		var html = ''+
+	var noMatch = function(){    // this sets up the function of noMatch with the results and two statements
+
+        var html = ''+
 			'<p>No Results found.</p>'+
-			'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
+			'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'    // "No results found and  Try searching for JavaScript. Just an idea with a font size of ten
 		;
-		resultsDIV.innerHTML = html;
+		resultsDIV.innerHTML = html;   // changes the variable to html
 	};
 	
 	// Put matches into page as paragraphs with anchors
