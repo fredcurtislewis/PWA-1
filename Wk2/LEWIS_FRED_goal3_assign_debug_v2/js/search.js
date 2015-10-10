7f9e865f49d02de4f1257e679fa9359a111f0e3a
Fred-Curtis <fredlewishg@gmail.com>
2015-10-09 15:40:51 -1000

    Wk 2 completed

--- Wk2/LEWIS_FRED_goal3_assign_debug_v2/js/search.js
+++ Wk2/LEWIS_FRED_goal3_assign_debug_v2/js/search.js
@@ -5,13 +5,10 @@
 (function() {
 
     // Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
-    var resultsDIV = document.getElementById("results"),
-        searchInput = document.forms[0].search,
-        currentSearch = ''
-        ;
+    var resultsDIV = document.getElementById("results"), searchInput = document.forms[0].search, currentSearch = '';
 
     // Validates search query
-    var validate = function (query) {
+     var validate = function(query){
 
         // Trim whitespace from start and end of search query
         while (query.charAt(0) == "") {
@@ -20,9 +17,8 @@
 
         while (query.charAt(query.length - 1) === "") {
             query = query.substring(0, query.length - 1);
-        };
-
-            // Check search length, must have 3 characters
+        }
+         // Check search length, must have 3 characters
             if (query.length < 3) {
                 alert("Your search query is too small, try again.");
 
@@ -30,17 +26,15 @@
                 searchInput.focus();
                 return;
             }
-
-
-            search(query);
+        search(query);
     };
 
 
         // Finds search matches
-        var search = function (query) {
+        var search = function(query){
 
             // split the user's search query string into an array
-            var queryArray = query.join(" ");
+            var queryArray = query.join();
 
             // array to store matched results from database.js
             var results = [];
@@ -51,12 +45,12 @@
                 // each db[i] is a single video item, each title ends with a pipe "|"
                 // save a lowercase variable of the video title
                 var dbTitleEnd = db[i].indexOf('|');
-                var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+                var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);
 
                 // loop through the user's search query words
                 // save a lowercase variable of the search keyword
                 for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {
-                    var qitem = queryArray[ii].tolowercase();
+                    var qitem = queryArray[ii].toLowerCase();
 
                     // is the keyword anywhere in the video title?
                     // If a match is found, push full db[i] into results array
@@ -83,10 +77,9 @@
 
         // Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
         var noMatch = function () {
-            var html = '' +
-                    "<p>No Results found.</p>" +
-                    "<p style=font-size:10px;>Try searching for \"JavaScript\".  Just an idea.</p>"
-                ;
+             html = '' +
+                    '<p>No Results found.</p>' +
+                    '<p style=font-size:10px;>Try searching for \"JavaScript\".  Just an idea.</p>';
             resultsDIV.innerHTML = html;
         };
 
@@ -104,8 +97,8 @@
 
                 // title of video ends with pipe
                 // pull the title's string using index numbers
-                titleEnd = results[i].indexOf('|');
-                title = results[i].subString(0, titleEnd);
+                var titleEnd = results[i].indexOf('|');
+                title = results[i].substring(0, titleEnd);
 
                 // pull the video url after the title
                 url = results[i].substring(results[i].indexOf('|') + 1, results[i].length);
@@ -118,13 +111,13 @@
 
         // The onsubmit event will be reviewed in upcoming Course Material.
         // THE LINE DIRECTLY BELOW IS CORRECT
-        document.forms[0].onsubmit = function () {
+        document.forms[0].onSubmit = function () {
             var query = searchInput.value;
-            validqte(query);
+            validate (query);
 
             // return false is needed for most events - this will be reviewed in upcoming course material
             // THE LINE DIRECTLY BELOW IS CORRECT
             return false;
         };
 
-})()
+})();
